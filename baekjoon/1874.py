1300a6bf81137293e78981e283ebcbf06d3ed57d
재용 <jaeyong@gangjingyeong-ui-MacBook-Pro.local>
2024-05-25 09:08:44 +0900

    백준 1874번. 스택 수열

---
+++ baekjoon/1874.py
@@ -0,0 +1,57 @@
+# 스택 수열
+
+"""
+예제 입력
+8
+4
+3
+6
+8
+7
+5
+2
+1
+"""
+
+import sys
+
+input = sys.stdin.readline
+
+# 입력
+n = int(input())  # 8
+sequence = list(int(input()) for _ in range(n))  # [4, 3, 6, 8, 7, 5, 2, 1]
+
+# 초기화
+stack = []
+result = []
+
+current = 1
+
+possible = True
+
+# 실행
+for number in sequence:
+    while current <= number:
+        stack.append(current)
+        result.append("+")
+        current += 1
+
+    if stack[-1] == number:
+        stack.pop()
+        result.append("-")
+    else:
+        possible = False
+        break
+
+if possible:
+    print("\n".join(result))
+else:
+    print("NO")
+
+"""
++ push 1 (current=1): stack = [1         ], result = ['+'                    ], current = 2
++ push 2 (current=2): stack = [1, 2      ], result = ['+', '+'               ], current = 3
++ push 3 (current=3): stack = [1, 2, 3   ], result = ['+', '+', '+'          ], current = 4
++ push 4 (current=4): stack = [1, 2, 3, 4], result = ['+', '+', '+', '+'     ], current = 5
+- pop  4            : stack = [1, 2, 3   ], result = ['+', '+', '+', '+', '-'], current = 5
+"""
