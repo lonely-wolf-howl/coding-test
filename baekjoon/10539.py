42de361b0c70101f298792ce13697eaabd4e44cd
kme-developer <electruc0095@gmail.com>
2024-12-07 16:09:49 +0900

    백준 10539번. 수빈이와 수열

---
+++ baekjoon/10539.py
@@ -0,0 +1,30 @@
+# 수빈이와 수열
+
+"""
+예제 입력
+4
+3 2 3 5
+"""
+
+# 내 풀이
+N = input()
+B = list(map(int, input().split()))
+
+A = []
+
+for i in range(len(B)):
+    temp = ((i + 1) * B[i]) - (i * B[i - 1])
+    A.append(temp)
+
+print(*A)
+
+# 해설
+N, B = int(input()), list(map(int, input().split()))
+
+A = [B[0]]
+
+for i in range(1, N):
+    A.append((i + 1) * B[i] - sum(A))
+
+for i in A:
+    print(i, end=' ')
