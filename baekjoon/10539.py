8c7c92275737ee7ec197ce219167e7a8e76aeda4
kme-developer <electruc0095@gmail.com>
2024-12-07 16:13:15 +0900

    fix. 10539 to better code.

--- baekjoon/10539.py
+++ baekjoon/10539.py
@@ -7,24 +7,25 @@
 """
 
 # 내 풀이
-N = input()
-B = list(map(int, input().split()))
-
-A = []
-
-for i in range(len(B)):
-    temp = ((i + 1) * B[i]) - (i * B[i - 1])
-    A.append(temp)
-
-print(*A)
+# N = input()
+# B = list(map(int, input().split()))
+#
+# A = []
+#
+# for i in range(len(B)):
+#     temp = ((i + 1) * B[i]) - (i * B[i - 1])
+#     A.append(temp)
+#
+# print(*A)
 
 # 해설
 N, B = int(input()), list(map(int, input().split()))
 
-A = [B[0]]
+A = [0 for i in range(len(B))]
+A[0] = B[0]
 
 for i in range(1, N):
-    A.append((i + 1) * B[i] - sum(A))
+    A[i] = ((i + 1) * B[i] - sum(A))
 
 for i in A:
     print(i, end=' ')
