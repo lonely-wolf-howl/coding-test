0093ad10632cc7e99d52c3d52472fc480c183c8e
kme-developer <electruc0095@gmail.com>
2025-01-01 15:36:13 +0900

    fix. stack

--- baekjoon/1012.py
+++ baekjoon/1012.py
@@ -8,32 +8,30 @@ dy = [0, 1, 0, -1]
 
 
 def dfs(x: int, y: int, grid: list, visited: list):
+    stack = [(x, y)]
     visited[x][y] = True
-    # 네 방향 탐색
-    for i in range(4):
-        # 새로운 좌표 계산
-        nx, ny = x + dx[i], y + dy[i]
-        if not visited[nx][ny] and grid[nx][ny] == 1:
-            dfs(nx, ny, grid, visited)
+
+    while stack:
+        cx, cy = stack.pop()
+        for i in range(4):
+            nx, ny = cx + dx[i], cy + dy[i]
+            if not visited[nx][ny] and grid[nx][ny] == 1:
+                visited[nx][ny] = True
+                stack.append((nx, ny))
 
 
 def process():
     M, N, K = map(int, input().split())
 
-    # 여백을 추가하여 격자 생성
     grid = [[0 for _ in range(M + 2)] for _ in range(N + 2)]
     visited = [[False for _ in range(M + 2)] for _ in range(N + 2)]
 
-    # 배추 위치 입력 (여백 때문에 +1씩 더해줌)
     for _ in range(K):
         X, Y = map(int, input().split())
-        # 배추가 있는 위치 표시 (Y+1, X+1)
         grid[Y + 1][X + 1] = 1
 
-    # 연결 요소의 개수 (필요한 지렁이 수)
     answer = 0
 
-    # 여백이 추가된 격자를 순회하며 DFS 실행
     for i in range(1, N + 1):
         for j in range(1, M + 1):
             if grid[i][j] == 1 and not visited[i][j]:
