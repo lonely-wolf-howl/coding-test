e92b391774a468943c8f1a352506cbb41909becf
kme-developer <electruc0095@gmail.com>
2025-01-05 22:44:38 +0900

    fix.

--- baekjoon/1233.py
+++ baekjoon/1233.py
@@ -1,13 +1,11 @@
 # 주사위
+# https://www.acmicpc.net/problem/1233
+
 
 """
 예제 입력
 3 2 3
 """
-
-import sys
-
-input = sys.stdin.readline
 
 S1, S2, S3 = map(int, input().split())
 counter = dict()
@@ -16,7 +14,7 @@ counter = dict()
 for i in range(1, S1 + 1):
     for j in range(1, S2 + 1):
         for k in range(1, S3 + 1):
-            sum = i + j + k  # 눈금의 합 게산
+            sum: int = i + j + k  # 눈금의 합 게산
             # 눈금의 합 등장 횟수 세기
             if sum not in counter:
                 counter[sum] = 1
@@ -28,7 +26,8 @@ max_count = -1
 answer = int(1e9)
 
 for key, value in counter.items():
-    if max_count < value:  # 등장 횟수가 더 많은 눈금의 합을 찾은 경우
+    # 등장 횟수가 더 많은 눈금의 합을 찾은 경우
+    if max_count < value:
         max_count = value
         answer = key
     elif max_count == value:
