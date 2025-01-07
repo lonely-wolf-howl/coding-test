d8665d718a35344617af50d1cf18dbd9d202e2ee
kme-developer <electruc0095@gmail.com>
2025-01-07 23:11:58 +0900

    fix.

--- baekjoon/1764.py
+++ baekjoon/1764.py
@@ -1,4 +1,5 @@
 # 듣보잡
+# https://www.acmicpc.net/problem/1764
 
 """
 예제 입력
@@ -10,32 +11,25 @@ obama
 baesangwook
 ohhenrie
 clinton
-
-예제 출력
-2
-baesangwook
-ohhenrie
 """
-
-import sys
-
-input = sys.stdin.readline
 
 from collections import defaultdict
 
 N, M = map(int, input().split())
 name = [input().strip() for _ in range(N + M)]
 
-# the dict is empty, but the first time a key is accessed, its value is initialized to 0.
-dict = defaultdict(int)
+# the name_count is empty, but the first time a key is accessed,
+# its value is initialized to 0
+name_count = defaultdict(int)
 answer = []
 
 for x in name:
-    dict[x] += 1
+    name_count[x] += 1
 
-for key in dict:
-    if dict[key] == 2:
+for key in name_count:
+    if name_count[key] == 2:
         answer.append(key)
+
 print(len(answer))
 
 for x in sorted(answer):
