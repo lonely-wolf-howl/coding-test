8c4120c567975b159a15bfcc1b5f19607f3e83cf
kme-developer <electruc0095@gmail.com>
2025-07-15 08:47:57 +0900

    fix.

--- baekjoon/1074.py
+++ baekjoon/1074.py
@@ -5,26 +5,33 @@
 2 3 1
 """
 
+n, r, c = map(int, input().split())
+
 
 def solve(n, x, y):
     global result
+
     if n == 2:
         if x == r and y == c:
             print(result)
             return
         result += 1
+
         if x == r and y + 1 == c:
             print(result)
             return
         result += 1
+
         if x + 1 == r and y == c:
             print(result)
             return
         result += 1
+
         if x + 1 == r and y + 1 == c:
             print(result)
             return
         result += 1
+
         return
 
     solve(n / 2, x, y)
@@ -33,8 +40,6 @@ def solve(n, x, y):
     solve(n / 2, x + n / 2, y + n / 2)
 
 
-N, r, c = map(int, input().split())
-
 result = 0
 
-solve(2**N, 0, 0)
+solve(2**n, 0, 0)
