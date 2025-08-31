da81dce192fedcc226c1be81b2d4fc5059be3da1
kme-developer <electruc0095@gmail.com>
2025-09-01 08:00:51 +0900

    fix.

--- baekjoon/2164.py
+++ baekjoon/2164.py
@@ -1,1 +1,20 @@
 # https://www.acmicpc.net/problem/2164
+
+from collections import deque
+
+n = int(input())
+
+deq = deque(list(range(1, n + 1)))
+
+drop = True
+
+while len(deq) > 1:
+    if drop:
+        deq.popleft()
+        drop = False
+    else:
+        x = deq.popleft()
+        deq.append(x)
+        drop = True
+
+print(deq[0])
