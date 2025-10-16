6a0fb9f0ce2566622a93b2425e42388571a82138
jay-kim <electruc@naver.com>
2025-10-16 15:24:39 +0900

    fix.

--- baekjoon/2417.py
+++ baekjoon/2417.py
@@ -13,12 +13,4 @@ while left < right:
     else:
         left = mid + 1
 
-"""
-left  right  mid  mid²  comparison
-   0     16    8   64      64 ≥ 16
-   0      8    4   16      16 ≥ 16
-   0      4    2    4       4 < 16
-   3      4    3    9       9 < 16
-"""
-
 print(left)
