259984eeed5f7372996baea260044b3129103996
jay-kim <electruc@naver.com>
2025-10-25 12:03:07 +0900

    add. brute force

---
+++ baekjoon/1182.py
@@ -0,0 +1,15 @@
+# https://www.acmicpc.net/problem/1182
+
+from itertools import combinations
+
+n, s = map(int, input().split())
+arr = list(map(int, input().split()))
+
+cnt = 0
+for r in range(1, n + 1):  # r: subset length
+    print(r)
+    for comb in combinations(arr, r):
+        if sum(comb) == s:
+            cnt += 1
+
+print(cnt)
