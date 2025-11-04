468fab9cf560defcad6762cb164be1a73505a3aa
jay-kim <electruc@naver.com>
2025-11-04 18:17:39 +0900

    fix.

--- baekjoon/1920.py
+++ baekjoon/1920.py
@@ -1,34 +1,10 @@
-# 수 찾기
 # https://www.acmicpc.net/problem/1920
 
-"""
-예제 입력
-5
-4 1 5 2 3
-5
-1 3 7 9 5
-"""
+a = set(map(int, input().split()))
+b = list(map(int, input().split()))
 
-# 풀이 1
-N = int(input())
-A = set(map(int, input().split()))
-M = int(input())
-B = list(map(int, input().split()))
-
-for x in B:
-    if x not in A:
+for x in b:
+    if x not in a:
         print("0")
     else:
         print("1")
-
-# 풀이 2
-N = (int(input()),)
-A = {i: 1 for i in map(int, input().split())}
-M = (int(input()),)
-B = list(map(int, input().split()))
-
-# print(A)
-# {4: 1, 1: 1, 5: 1, 2: 1, 3: 1}
-
-for x in B:
-    print(A.get(x, 0))
