729c109a44488b4acb50d83ef1243c5428ea6664
jay-kim <electruc@naver.com>
2025-11-05 11:23:58 +0900

    fix. stack

--- baekjoon/1874.py
+++ baekjoon/1874.py
@@ -1,51 +1,28 @@
-# 스택 수열
 # https://www.acmicpc.net/problem/1874
 
-"""
-예제 입력
-8
-4
-3
-6
-8
-7
-5
-2
-1
-"""
-
-n = int(input())  # 8
-sequence = list(int(input()) for _ in range(n))  # [4, 3, 6, 8, 7, 5, 2, 1]
+n = int(input())
+sequence = [int(input()) for _ in range(n)]
 
 stack = []
-result = []
+operations = []
 current = 1
-possible = True
 
-for number in sequence:
-    while current <= number:
+is_possible = True
+
+for target in sequence:
+    while current <= target:
         stack.append(current)
-        result.append("+")
+        operations.append("+")
         current += 1
 
-    if stack[-1] == number:
+    if stack[-1] == target:
         stack.pop()
-        result.append("-")
+        operations.append("-")
     else:
-        possible = False
+        is_possible = False
         break
 
-if possible:
-    print("\n".join(result))
+if is_possible:
+    print("\n".join(operations))
 else:
     print("NO")
-
-"""
-+ push 1 (current=1): stack = [1         ], result = ['+'                              ]
-+ push 2 (current=2): stack = [1, 2      ], result = ['+', '+'                         ]
-+ push 3 (current=3): stack = [1, 2, 3   ], result = ['+', '+', '+'                    ]
-+ push 4 (current=4): stack = [1, 2, 3, 4], result = ['+', '+', '+', '+'               ]
-- pop  4            : stack = [1, 2, 3   ], result = ['+', '+', '+', '+', '-'          ]
-- pop  3            : stack = [1, 2,     ], result = ['+', '+', '+', '+', '-', '-'     ]
-+ push 5 (current=5): stack = [1, 2, 5   ], result = ['+', '+', '+', '+', '-', '-', '+']
-"""
